5f155481e5a05f921c002c9341ada2014e75db44
mfulp2020 <mfulp2020@gmail.com>
2026-01-08 15:18:14 -0600

    Delete DiamondJ directory

--- DiamondJ/src/app/sitemap.ts
+++
@@ -1,9 +0,0 @@
-import { site } from "@/lib/site";
-
-export default function sitemap() {
-  const pages = ["", "/menu", "/catering", "/gallery", "/order", "/contact"];
-  return pages.map((p) => ({
-    url: `${site.url}${p}`,
-    lastModified: new Date(),
-  }));
-}
